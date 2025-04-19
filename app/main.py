104b31b900ecb04e715eaa894b70499ee1ceac2f
Roman Azhniuk <roman.azhniuk@gmail.com>
2025-04-19 22:24:39 +0300

    implement py-feed-animals

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
+from typing import List
+
+
 class Animal:
-    def __init__(self, name, appetite, is_hungry=True):
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self):
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self):
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
             self.is_hungry = False
@@ -16,22 +19,22 @@ class Animal:
 
 
 class Cat(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 3, is_hungry)
 
-    def catch_mouse(self):
+    def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
 class Dog(Animal):
-    def __init__(self, name, is_hungry=True):
+    def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, 7, is_hungry)
 
-    def bring_slippers(self):
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
-def feed_animals(animals):
+def feed_animals(animals: List[Animal]) -> int:
     total = 0
     for animal in animals:
         total += animal.feed()
